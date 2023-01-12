cd75b111e4904f8924d3ac01df0ca0d643683518
4e6yPek48 <matchtek48@gmail.com>
2023-01-12 15:33:03 +0300

    четрые полосы, разные направления. фикс некоторых багов.

--- Objects.py
+++ Objects.py
@@ -52,15 +52,64 @@ class EnemyCar(pygame.sprite.Sprite):
             self.truck = False
 
     def update(self, *args):
-        self.rect.left -= 5
+        self.rect.left -= 6
         if pygame.sprite.spritecollideany(self, main_car) and self.truck is False:
             self.image = pygame.image.load('sprites/destroyed_enemy_car.png').convert_alpha()
         if pygame.sprite.spritecollideany(self, main_car) and self.truck is True:
             self.image = pygame.image.load('sprites/destroyed_truck.png').convert_alpha()
+        if self.rect.x in range(-100, -50):
+            self.rect.x = random.randint(1500, 1900) + random.randint(-300, 400)
 
     def stop(self, *args):
-        self.rect.x += 5
+        self.rect.x += 6
 
+
+class EnemyCar2(pygame.sprite.Sprite):
+    def __init__(self, pos, x, cr):
+        if cr == 'c1':
+            super().__init__(cars1)
+            self.add(cars1)
+        if cr == 'c2':
+            super().__init__(cars2)
+            self.add(cars2)
+        if cr == 'c3':
+            super().__init__(cars3)
+            self.add(cars3)
+        if cr == 'c4':
+            super().__init__(cars4)
+            self.add(cars4)
+
+        self.truck = False
+        self.car = random.randint(0, 1)
+        if self.car == 0:
+            self.image = pygame.image.load('sprites/truck2.png').convert_alpha()
+            self.rect = self.image.get_rect()
+            self.rect.x = random.randint(1500, 1900) + random.randint(-300, 400)
+            self.rect.y = 295 + x
+            self.truck = True
+        else:
+            self.image = pygame.image.load('sprites/enemy_car2.png').convert_alpha()
+            self.rect = self.image.get_rect()
+            self.rect.x = random.randint(1500, 1900) + random.randint(-300, 400)
+            self.rect.y = 300 + x
+            self.truck = False
+
+    def update(self, *args):
+        self.rect.left -= 4
+        if pygame.sprite.spritecollideany(self, main_car) and self.truck is False:
+            self.image = pygame.image.load('sprites/destroyed_enemy_car2.png').convert_alpha()
+        if pygame.sprite.spritecollideany(self, main_car) and self.truck is True:
+            self.image = pygame.image.load('sprites/destroyed_truck2.png').convert_alpha()
+        if self.rect.x in range(-500, -450):
+            self.rect.x = random.randint(1500, 1900) + random.randint(-300, 400)
+        if self.rect.x in range(2350, 2500):
+            self.rect.x = random.randint(-300, -100) + random.randint(-100, 100)
+
+    def stop(self, *args):
+        self.rect.x += 4
+
+    def revert(self, *args):
+        self.rect.x += 8
 
 
 class MainCar(pygame.sprite.Sprite):
@@ -101,4 +150,4 @@ class Road(pygame.sprite.Sprite):
         self.rect.y = 350
 
     def update(self, *args):
-        self.rect.left -= 3.5
+        self.rect.left -= 5
